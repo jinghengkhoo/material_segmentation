55e460e046bbbe9fe266ac08162a1aefb641c0e1
jinghengkhoo <jingheng@graymatics.com>
2022-05-27 06:56:33 +0000

    crop fix

--- get_cropped_object.py
+++ get_cropped_object.py
@@ -1,18 +1,9 @@
 
 import json 
 import numpy as np
-import matplotlib.pyplot as plt
-from PIL import Image
 import cv2
 
-f = open('room.json')
-orig_img = cv2.imread('room.jpg')
-mask = np.zeros_like(orig_img) 
 
-data = json.load(f)
-
-categories = data['categories']
-annots = data['annotations']
 
 def make_contour(segments_pts):
     points_arr = []
@@ -32,23 +23,54 @@ def make_contour(segments_pts):
     
     return points
 
-
-for i in range(len(annots)):
-    segments_Points=annots[i]['segmentation']
-    category_id = annots[i]['category_id']
-    crop_region = categories[category_id]['name']
+def get_crops(json_mask, orig_img):
     mask = np.zeros_like(orig_img) 
 
-    for segments in range(len(segments_Points)):
-        
-        pts = make_contour(segments_Points[segments])
-                
-        cv2.fillPoly(mask, np.int32([pts]), color = (255,255,255))
-        result = cv2.bitwise_and(orig_img, mask)
-        change_mask = result[:,:,2] == 0
-        result[change_mask] = [255,255,255]
+    categories = json_mask['categories']
+    annots = json_mask['annotations']
 
-        cv2.imwrite('results/'+ str(crop_region) + '.jpg', result)
+    height, width, _ = orig_img.shape
+
+    crops = {}
+    bbox = {}
+
+    for i in range(len(annots)):
+        segments_Points=annots[i]['segmentation']
+        category_id = annots[i]['category_id']
+        crop_region = categories[category_id]['name']
+        mask = np.zeros_like(orig_img) 
+
+        for segments in range(len(segments_Points)):
+            
+            pts = make_contour(segments_Points[segments])
+                    
+            cv2.fillPoly(mask, np.int32([pts]), color = (255,255,255))
+            result = cv2.bitwise_and(orig_img, mask)
+            change_mask = result[:,:,2] == 0
+            result[change_mask] = [255,255,255]
+            crops[crop_region] = result
+
+    for name, crop in crops.items():
+        out = np.zeros((height, width), np.uint16)
+        for i in range(1, height): 
+            for j in range(1, width):
+                if (np.any(crop[i][j] < 255)):
+                    out[i][j] = min(out[i][j-1], out[i-1][j], out[i-1][j-1]) + 1
+                else:
+                    out[i][j] = 0
+        ind = np.unravel_index(np.argmax(out, axis=None), out.shape)
+        new_y, new_x = (ind)
+        side = out[ind]
+        result_2 = orig_img[new_y-side:new_y, new_x-side:new_x]
+        crops[name] = result_2
+        bbox[name] = {
+				"x1": int(new_x-side),
+				"x2": int(new_x),
+				"y1": int(new_y-side),
+				"y2": int(new_y)
+		}
+    
+    return crops, bbox
 
         
 
